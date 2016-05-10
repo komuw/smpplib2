934aaf82f3ca0e8d6ef17caeaa87d6c94bc2a6a4
komu <komuw05@gmail.com>
2016-05-10 20:02:20 +0300

    update setup

--- setup.py
+++ setup.py
@@ -6,22 +6,15 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
 setup(name="smpplib2",
-      version='1.0.1',
-      url='https://github.com/podshumok/python-smpplib2',
+      version='0.1',
+      url='https://github.com/komuW/smpplib2',
       description='SMPP library for python',
       packages=find_packages(),
       zip_safe=True,
       classifiers=[
-        'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Topic :: Communications :: Telephony',
-        'Intended Audience :: Telecommunications Industry',
         'License :: MIT License',
         ],
       **extra
