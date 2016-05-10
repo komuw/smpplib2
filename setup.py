776a83bbe15e1093b59939e0b97c6373fdc3bb52
komu <komuw05@gmail.com>
2016-05-10 21:43:52 +0300

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
 setup(name="smpplib2",
-      version='0.1.0',
+      version='0.1.1',
       url='https://github.com/komuW/smpplib2',
       description='SMPP library for python',
       packages=find_packages(),
@@ -17,5 +17,10 @@ setup(name="smpplib2",
         'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: MIT License',
         ],
+      description='A python smpplib client library',
+      long_description='Python lib to enable you to create clients(ESME) that speak SMPP',
+      author='komuW',
+      author_email='komuw05@gmail.com',
+      keywords='smpplib smpplib2 python-smpplib ESME SMC PDU',
       **extra
 )
